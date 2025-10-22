67b48bab789550d28ea1eb984b4a2e8f1afb4d0e
sanjaynakum90 <sanjaynakum4545@gmail.com>
2025-10-22 16:15:17 +0530

    update

--- 02-react-Movie-api/src/Components/MovieList.jsx
+++ 02-react-Movie-api/src/Components/MovieList.jsx
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import MovieCard from './MovieCard';
+import React from 'react';
+import MovieCard from './MovieCard';
 
-// const MovieList = ({ movies }) => (
-//     <div className="row">
-//         {movies.map((movie) => (
-//             <div className="col-md-4" key={movie.imdbID}>
-//                 <MovieCard movie={movie} />
-//             </div>
-//         ))}
-//     </div>
-// );
+const MovieList = ({ movies }) => (
+    <div className="row">
+        {movies.map((movie) => (
+            <div className="col-md-4" key={movie.imdbID}>
+                <MovieCard movie={movie} />
+            </div>
+        ))}
+    </div>
+);
 
-// export default MovieList;
+export default MovieList;
